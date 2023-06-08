b1faabb61a78b2621dd8804971fa03ad86eb540b
王超 <wangchao1.vendor@senseauto.com>
2023-06-08 11:28:22 +0800

    feat:  优化打包产物提交
    1.1MB -> 800kb

--- vite.config.ts
+++ vite.config.ts
@@ -24,21 +24,11 @@ if (fs.existsSync(".git")) {
 export default defineConfig({
   build: {
     outDir: "dist",
-    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         chunkFileNames: "js/[name]-[hash].js",
         entryFileNames: "js/[name]-[hash].js",
         assetFileNames: "[ext]/[name]-[hash].[ext]",
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
       },
     },
   },
